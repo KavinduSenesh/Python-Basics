d123a20d01d3c5b3eddd69cf0ef1a0f7adbf979e
Kavindu Senesh <ksenesh5@gmail.com>
2024-11-09 09:17:14 +0530

    Add initial Python scripts and Jupyter notebook for basic variable and list operations

---
+++ day_01/app.py
@@ -0,0 +1,10 @@
+a = 10;
+#The class of a is int
+print(a);
+print(type(a));
+
+#we can parse unlimited number of arguments to print func tion using comma
+print(a,type(a));
+print("The type of variable having", a ,"is" , type(a));
+
+a = 2.5e4
